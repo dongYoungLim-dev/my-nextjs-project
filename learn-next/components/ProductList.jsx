f03a42f06858fcaf185683f35750247cc4e8e675
dy_lim <ldy6740@gmail.com>
2025-12-18 13:52:17 +0900

    create ui and components add

---
+++ learn-next/components/ProductList.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function ProductList() {
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/products').then(response => {
+      setProducts(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {products &&
+          products.map(product => <li key={product.id}>{product.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+export default ProductList;
--- learn-next/pages/index.jsx
+++ learn-next/pages/index.jsx
@@ -1,5 +1,14 @@
+import ProductList from '@/components/ProductList';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function ProductPage() {
-  return <div>상품 페이지</div>;
+  return (
+    <div>
+      <h1>상품목록 페이지</h1>
+      <ProductList></ProductList>
+    </div>
+  );
 }
 
 export default ProductPage;
